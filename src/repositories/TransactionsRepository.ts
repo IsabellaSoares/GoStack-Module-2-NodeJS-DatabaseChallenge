c5d708232a273c7a25c621ae4e0a46132391520f
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-13 17:41:34 -0300

    calculate balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,7 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from './TransactionsRepository';
 
 interface Balance {
   income: number;
@@ -10,9 +11,29 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<void> {
-    // public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(): Promise<Balance> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const incomeArray = await transactionsRepository.find({
+      where: { type: 'income' },
+    });
+
+    const income = incomeArray.reduce((acc, current) => acc + current.value, 0);
+
+    const outcomeArray = await transactionsRepository.find({
+      where: { type: 'outcome' },
+    });
+
+    const outcome = outcomeArray.reduce(
+      (acc, current) => acc + current.value,
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
