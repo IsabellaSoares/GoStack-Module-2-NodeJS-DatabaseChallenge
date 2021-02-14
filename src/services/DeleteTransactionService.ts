56436dea8817e74e064a4551c9b4235c28222d22
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-13 23:49:00 -0300

    delete transactions

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const checkTransactionExists = await transactionsRepository.findOne(id);
+
+    if (!checkTransactionExists) {
+      throw new AppError('This transaction does not exist.');
+    }
+
+    transactionsRepository.delete(id);
   }
 }
 
